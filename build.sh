88155aa61436f4d2fd4af866d1fca5fdb76d5ed4
wessel <mastwessel@gmail.com>
2021-02-03 14:41:05 +0100

    changed build/run files

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ defines="-D__DEBUG__ -g"
 includes="-L/usr/X11/lib -L/usr/include/GL" 
 libraries="-lX11 -lGLEW -lGL -lGLU"
 
-g++ -o bin/out src/Linux.cpp $defines $includes $libraries
+g++ -o bin/linux src/Linux.cpp $defines $includes $libraries
